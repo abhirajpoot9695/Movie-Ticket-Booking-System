b539412623a7b01f7b9b75668527d18b79905112
Abhishek Rajpoot <abhirajpoot9695@gmail.com>
2026-03-28 01:26:20 +0530

    Create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeaturesSection from '../components/FeaturesSection'
+import TrailersSection from '../components/TrailersSection'
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <TrailersSection />
+    </>
+  )
+}
+
+export default Home
